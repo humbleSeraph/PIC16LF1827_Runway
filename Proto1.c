37c284b69cec1b1016ea2deb66bbcdb63ace9891
humbleSeraph <OsagieIgbeare@dn2lk5m42.stanford.edu>
2014-08-05 16:23:24 -0700

    Revision 1
    after looking through quick start guide for HI TECH Compiler, changed
    the code so that it should load onto PIC now

--- Proto1.c
+++ Proto1.c
@@ -10,22 +10,23 @@ author: Osagie Igbeare
 
 /**************** Header Files *********************/
 
-#include "bitdefs.h"
+#include <bitdefs.h>
 #include <htc.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <termio.h>
+
+
+/***************** Configuration Macros ***************/
+
+__CONFIG(FOSC_XT & WDTE_OFF & MCLRE_ON & PWRTE_OFF & BOREN_OFF
+		& LVP_ON & WRT_OFF & CPD_OFF & CP_OFF);
+
 
 /***************** # Defines *****************/
-#define LF1827_XT 0xFFF9;
-#define LF1827_Hi_Volt_Prgm 0xDFFF;
 
 /*************** module level variables ************/
 
 
 /******* Function Prototypes ***************/
 
-void ConfigSetUp(void);
 void InitPorts(void);
 void InitTimers(void);
 void InitInterrupts(void);
@@ -33,13 +34,8 @@ void InitInterrupts(void);
 
 /******* Acutal Functions ****************/
 
-void ConfigSetUp(){
-
-	CONFIG1 &= LF1827_XT;				// set Fosc to Medium Gain resonator/crystal (up to 4MHz)
-	CONFIG2 &= LF1827_Hi_Volt_Prgm;		// MCLR pin must be raised to 8V to repgrom LF1827
-}
-
-void InitPorts(){
+void InitPorts()
+{
 
 	ANSELA = 0x00;			// Port A pins are digital
 	ANSELB = 0x00;			// Port B pins are digital
@@ -52,28 +48,32 @@ void InitPorts(){
 
 }
 
-void InitTimers(){
+void InitTimers()
+{
 
 	T2CON = 0b01111110;		// Fosc / (4 instruct * 16 prescale * 16 postscale * 60 PR2) = 65 Hz
 	PR2 = 60; 
 }
 
-void InitInterrupts(){
+void InitInterrupts()
+{
 
 	INTCON = 0b11000000;	// Enable GIE, Enable PEIE
 	PIE1 = 0b00000010; 		// Enable TMR2IE, interrupt when Timer 2 matches PR2
 }
 
-void interrupt ISR(){
+void interrupt ISR()
+{
 
 	INTCON &= 0b00111111;   // Disable Interrupts
 
-	if (TMR2IF){
+	if (TMR2IF)
+	{
 
 		PORTA &= BIT2LO;	// turn LED on RA2 on by pulling pin low
 		PORTB &= BIT5LO; 	// turn LED on RB6 on
 
-		PIR1 &= BIT2LO;		// clears the TIMR2IF (timer 2 interrupt flag)
+		TMR2IF = 0;		// clears the TIMR2IF (timer 2 interrupt flag)
 
 	}
 
@@ -86,18 +86,14 @@ void interrupt ISR(){
 
 /******** Main - which actually runs the code ***********/
 
-void main (){
+void main ()
+{
 
 	// Initializing PIC16LF1827
-	ConfigSetUp();
 	InitPorts();
 	InitTimers();
 	InitInterrupts();
 
-	while (true)
-	{
-		// some code will go here, I think...
-	}
 
 
 }
